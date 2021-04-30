3e40292d972218e1bae402fc901ada79534b179b
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-05-01 00:06:45 +0300

    Fix Loyalty

--- Session/session_s/serializers.py
+++ Session/session_s/serializers.py
@@ -1,10 +1,5 @@
 from rest_framework import serializers
 from .models import Users
-from rest_framework_jwt.settings import api_settings
-
-
-JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
-JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 
 
 class UserSerializer(serializers.ModelSerializer):
