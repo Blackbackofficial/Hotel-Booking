f232c3ff4c6d5b48f493db6d064f7f5996507a4a
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-05-05 01:08:45 +0300

    initial setup

---
+++ Gateway_Service/gateway/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from .views import about_or_delete, all_hotels_or_add_hotel, change_rooms
+
+urlpatterns = [
+    # path('/<str:hotel_uid>', about_or_delete),
+    # path('', all_hotels_or_add_hotel),
+    # path('/<str:hotel_uid>/rooms', change_rooms),
+]
--- Gateway_Service/gateway/views.py
+++ Gateway_Service/gateway/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed, ValidationError, ParseError
+from circuitbreaker import circuit
+from rest_framework.decorators import api_view
+from Hotel_Service.settings import JWT_KEY
+from django.forms.models import model_to_dict
+from django.core import serializers
+from django.http import JsonResponse
+from rest_framework import status
+from .models import Hotels
+import requests
+import json
+import jwt
 
-# Create your views here.
+FAILURES = 3
+TIMEOUT = 6
+
+
+# API
+@circuit(failure_threshold=FAILURES, recovery_timeout=TIMEOUT)
+@api_view(['GET', 'DELETE'])
+def about_or_delete(request, hotel_uid):
+    request
