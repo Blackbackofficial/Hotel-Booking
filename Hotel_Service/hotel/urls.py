98d70a7f04beac5b2cd31f574d417e007bd4bf66
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-06-22 02:32:39 +0300

    Fix urls hotel

--- Hotel_Service/hotel/urls.py
+++ Hotel_Service/hotel/urls.py
@@ -4,7 +4,7 @@ from .views import about_or_delete, all_hotels_or_add_hotel, change_rooms, citie
 urlpatterns = [
     path('cities', cities),  #
     path('date', filter_date),
-    path('<str:hotel_uid>', about_or_delete),  #
     path('', all_hotels_or_add_hotel),  #
+    path('<str:hotel_uid>', about_or_delete),  #
     path('<str:hotel_uid>/rooms', change_rooms),
 ]
