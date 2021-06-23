90417e3c41695aefffcb46b3a294b9237ccd7413
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-06-23 04:13:22 +0300

    Add new field date_create

--- Booking_Service/booking/models.py
+++ Booking_Service/booking/models.py
@@ -7,6 +7,7 @@ class Reservations(models.Model):
     hotel_uid = models.UUIDField(default=uuid.uuid4, editable=True)
     user_uid = models.UUIDField(default=uuid.uuid4, editable=True)
     payment_uid = models.UUIDField(default=uuid.uuid4, editable=True, unique=True)
+    date_create = models.DateTimeField(auto_now=True)
     date_start = models.DateField(auto_now=False)
     date_end = models.DateField(auto_now=False)
     comment = models.CharField(max_length=255, blank=True)
