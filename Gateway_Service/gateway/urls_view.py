c018c7f02a2440be898113ff281a9bb52dfb3e37
MrsOwlya <olja-holmes@mail.ru>
2021-06-24 02:54:41 +0300

    Fancybox hotel image

--- Gateway_Service/gateway/urls_view.py
+++ Gateway_Service/gateway/urls_view.py
@@ -5,12 +5,18 @@ from .views import index, make_login, make_logout, registration, balance, add_ho
 
 urlpatterns = [
     # VIEW
-    path('index', index, name="index"),
+    path('index/', index, name="index"),
     path('login', make_login, name="login"),
     path('logout', make_logout, name="logout"),
     path('signup', registration, name="signup"),
     path('balance', balance, name="balance"),
+    path('search/', search_hotel_booking, name="search"),
+    path('add_booking', add_booking, name="add_booking"),
+    path('booking_info/<str:booking_uid>', booking_info, name="booking_info"),
+    path('pay_room/<str:payment_uid>', pay_room, name="pay_room"),
+    path('del_booking/<str:booking_uid>', del_booking, name="del_booking"),
     path('add-hotel', add_hotel_admin, name="add_hotel"),
+    path('hotel_info/<str:hotel_uid>', hotel_info, name="hotel_info"),
     path('admin', admin, name="admin"),
     path('delete-hotel', delete_hotel_admin, name="delete_hotel"),
     path('all-users', all_users, name="all_users"),
