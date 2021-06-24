283ee526b7602a21ecdc2f9a4af84b5650c47332
Blackback <chernov-ivan.1997@yandex.ru>
2021-06-24 23:51:55 +0300

    Fix settins && add plsq to report

--- Booking_Service/Booking_Service/settings.py
+++ Booking_Service/Booking_Service/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'booking',
         'USER': 'ivan',
         'PASSWORD': 'password',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
